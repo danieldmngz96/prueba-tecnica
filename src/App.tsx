ed236949a0e96ae1d6850f572817bd300bbb2840
Daniel Dominguez Dominguez <danarturodo@hotmail.com>
2024-02-07 19:38:57 -0500

    login full

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import { AuthContext } from './app/views/store/contexts/AuthContext'
-import './App.css';
-import { useReducer } from 'react';
-import { authReducer } from './app/views/store/reducers/authReducer';
-import { AppRouter } from './app/router/AppRouter';
+import { useReducer } from "react";
+import { AppRouter } from "./app/router/AppRouter";
+import { AuthContext } from "./app/views/store/contexts/AuthContext";
+import { authReducer } from "./app/views/store/reducers/authReducer";
 
 const init = () => {
-  let sessionUser: any = sessionStorage.getItem('user');
-  let user: any
+  let sessionUser: any = sessionStorage.getItem("user");
+  let user: any;
   if (!sessionUser) {
-    user = sessionUser
+    user = sessionUser;
   } else {
     user = JSON.parse(sessionUser);
   }
   return user;
-}
+};
 
 function App() {
-  const [user , dispatchUser ] = useReducer(authReducer, {}, init)
-  useReducer(authReducer, {}, init)
+  const [user, dispatchUser] = useReducer(authReducer, {}, init);
+
   return (
-    <AuthContext.Provider value={{user, dispatchUser}}>
-      <AppRouter/>
+    <AuthContext.Provider value={{ user, dispatchUser }}>
+      <AppRouter />
     </AuthContext.Provider>
   );
 }
--- src/app/router/AppRouter.tsx
+++ src/app/router/AppRouter.tsx
@@ -1,7 +1,35 @@
+import { useContext } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import { AuthRouter } from '../views/auth/AuthRouter';
+import { DashboardRouter } from '../views/dashboard/DashboardRouter';
+import { AuthContext } from '../views/store/contexts/AuthContext';
+import { PrivateRouter } from './PrivateRouter';
+
+interface Context {
+   dispatchUser?:any,
+   user?:User
+}
+
+interface User{
+   loggedIn:boolean
+}
+
+
 export function AppRouter(){
-    return (
-        <div>
-            
-        </div>
-    );
+
+   const { user }:Context = useContext(AuthContext);
+
+   return (
+      <Router>
+         <Switch>
+            <Route path='/auth' component={AuthRouter} />
+            <PrivateRouter
+              loggedIn={user?.loggedIn}
+              component={DashboardRouter}
+            />
+
+            <Redirect to='/dashboard/home' />   
+         </Switch>
+      </Router>
+   );
 }
